df82b0aca4dc6a41166d08a36ed712762c317bf7
Isaac <moatsisaac@icloud.com>
2026-04-07 13:20:07 -0600

    websocketfacade constructor:

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -1,7 +1,39 @@
 package client.websocket;
+import com.google.gson.Gson;
+import exception.DataAccessException;
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
 
     Session session;
+    NotificationHandler notificationHandler;
 
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws DataAccessException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new DataAccessException(ex.getMessage(), 400);
+        }
+    }
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
 }
